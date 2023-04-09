dd2eb76ca107e930d38e58ff8ce0dea4bd1385c5
Crowdbotics <team@crowdbotics.com>
2023-04-09 00:47:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-wildflower-39798",
+  url: "https://tight-wildflower-39798.botics.co",
+  api: "https://tight-wildflower-39798.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
